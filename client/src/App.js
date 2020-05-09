9e27b2d6157a58244d899bc08bcc834d6a851685
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-05-09 15:22:40 -0400

    added a Route with a parameter in the path

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { Route } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -35,6 +36,10 @@ const App = () => {
       <Route 
         exact path='/' 
         render={(props) => <MovieList {...props} movies={movieList}/>} 
+      />
+      <Route 
+        path='/movies/:id'
+        component={Movie}
       />
     </div>
   );
