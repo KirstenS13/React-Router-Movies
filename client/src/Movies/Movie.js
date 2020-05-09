247fe670cc491718b2822aac4e3b17b07318621a
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-05-09 16:44:25 -0400

    got useParams to work, it now links to the correct movie

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+//import useParams
+import { useParams } from 'react-router-dom';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
+
+  //use useParams
+  const params = useParams();
+  console.log(params);
  
   useEffect(() => {
-    const id = 1;
+    const id = params.id;
+    console.log('id', id);
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
